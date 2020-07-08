b6fec5c0df7add4478c1a78a9748f3d65dae163f
rohan <rohanrajgautam@gmail.com>
2020-07-08 22:33:05 +0545

    Updates

---
+++ precache-manifest.7e8cb644ccc3e166dcf764622f2783ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca17be5fca1011b63fcb71ff85c2794",
+    "url": "/react-tic.tac.toe/index.html"
+  },
+  {
+    "revision": "015aa23d2861a3e60a9c",
+    "url": "/react-tic.tac.toe/static/css/main.327b8c50.chunk.css"
+  },
+  {
+    "revision": "aec9074afba8e3d9dfa3",
+    "url": "/react-tic.tac.toe/static/js/2.d9b0a751.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic.tac.toe/static/js/2.d9b0a751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015aa23d2861a3e60a9c",
+    "url": "/react-tic.tac.toe/static/js/main.aa547129.chunk.js"
+  },
+  {
+    "revision": "f77d25229ec316d2051d",
+    "url": "/react-tic.tac.toe/static/js/runtime-main.35a00f32.js"
+  }
+]);
